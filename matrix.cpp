7adfbc0be0895714a6039b805e4dcb18dc550908
Anastasia Yakushyna <ayakushina07@gmail.com>
2019-12-25 17:49:16 +0200

    Added random fill

--- matrix.cpp
+++ matrix.cpp
@@ -16,6 +16,7 @@ Matrix::Matrix(int n, QWidget *parent)
         {
             QLineEdit *newItem = new QLineEdit("0",this);
             newItem->setValidator( new QIntValidator(0, n*n, this) );
+            newItem->setAlignment(Qt::AlignCenter);
             setCellWidget (i, j, newItem);
         }
     }
@@ -41,6 +42,38 @@ Matrix::Matrix(int n, const std::vector<std::vector<int>>& matrix, QWidget *pare
         {
             QLineEdit *newItem = new QLineEdit(QString::number(matrix[i][j]), this);
             newItem->setValidator( new QIntValidator(0, n*n, this) );
+            newItem->setAlignment(Qt::AlignCenter);
+            setCellWidget(i, j, newItem);
+        }
+    }
+}
+
+void Matrix::setMatrix(int n){
+    m_size = n;
+    setRowCount(n);
+    setColumnCount(n);
+
+    for(int i = 0; i < n; ++i){
+        for(int j = 0; j < n; ++j)
+        {
+            QLineEdit *newItem = new QLineEdit("0",this);
+            newItem->setValidator( new QIntValidator(0, n*n, this) );
+            newItem->setAlignment(Qt::AlignCenter);
+            setCellWidget (i, j, newItem);
+        }
+    }
+}
+void Matrix::setMatrix(int n, const std::vector<std::vector<int>>& matrix){
+    m_size = n;
+    setRowCount(n);
+    setColumnCount(n);
+
+    for(int i = 0; i < n; ++i){
+        for(int j = 0; j < n; ++j)
+        {
+            QLineEdit *newItem = new QLineEdit(QString::number(matrix[i][j]), this);
+            newItem->setValidator( new QIntValidator(0, n*n, this) );
+            newItem->setAlignment(Qt::AlignCenter);
             setCellWidget(i, j, newItem);
         }
     }
@@ -79,6 +112,29 @@ void Matrix::findBlankItem(int&x, int&y){
             {
                 x = i; y = j;
             }
+
+        }
+    }
+}
+
+void Matrix::fillRandom(){
+    std::vector<int> numbers(m_size*m_size);
+
+    for(int i = 0; i < m_size*m_size; ++i){
+        numbers[i] = i;
+    }
+
+    for(int i = 0; i < m_size; ++i){
+        for(int j = 0; j < m_size; ++j)
+        {
+            int k = (numbers.size() - 1) > 0? gen.bounded(static_cast<int>(numbers.size() - 1)) : 0;
+
+            QLineEdit *newItem = new QLineEdit(QString::number(numbers[k]), this);
+            newItem->setValidator( new QIntValidator(0, m_size*m_size, this) );
+            newItem->setAlignment(Qt::AlignCenter);
+            setCellWidget(i, j, newItem);
+
+            numbers.erase(numbers.begin() + k);
 
         }
     }
--- matrix.h
+++ matrix.h
@@ -7,19 +7,26 @@
 #include <QTableWidget>
 #include <QHeaderView>
 #include <QLineEdit>
-
+#include <QRandomGenerator>
 class Matrix : public QTableWidget
 {
 public:
     Matrix(int n, QWidget *parent = nullptr);
     Matrix(int n, const std::vector<std::vector<int>>& matrix, QWidget *parent = nullptr);
+
+    void setMatrix(int n);
+    void setMatrix(int n, const std::vector<std::vector<int>>& matrix);
     std::vector<std::vector<int>> getMatrix() const ;
+
     bool checkMatrix() const;
     void findBlankItem(int&x, int&y);
+    void fillRandom();
+
     ~Matrix();
 
 private:
     int m_size;
+    QRandomGenerator gen;
 };
 
 #endif // MATRIX_H
